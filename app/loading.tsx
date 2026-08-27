0cb5bdc8681ec2230ee87d231fb3034b73c4af5a
DesintaRahma <ciintaaa57@gmail.com>
2026-08-27 14:26:28 +0700

    first commit

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">    
+        <div className="text-x1 font-semibold text-gray-700">
+            sedang memuat data... 
+            </div>
+        </div>
+    );
+}
